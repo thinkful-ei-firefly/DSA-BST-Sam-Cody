2165031e31ce144d474c3f404231ee7a84592846
krill <lpmaze@gmail.com>
2019-10-08 14:38:42 -0400

    done w 7

--- drills.js
+++ drills.js
@@ -1,10 +1,10 @@
 class BinarySearchTree {
-  constructor(key=null, value=null, parent=null) {
-    this.key=key
-    this.value=value
-    this.parent=parent
-    this.left=null
-    this.right=null
+  constructor(key = null, value = null, parent = null) {
+    this.key = key
+    this.value = value
+    this.parent = parent
+    this.left = null
+    this.right = null
   }
   insert(item) {
     if (this.key === null) {
@@ -15,8 +15,7 @@ class BinarySearchTree {
       } else {
         this.left.insert(item)
       }
-      
-    } else if (item.key > this.key){
+    } else if (item.key > this.key) {
       if (this.right === null) {
         this.right = new BinarySearchTree(item.key, item.value, this)
       } else {
@@ -30,24 +29,29 @@ class BinarySearchTree {
     if (this.key === null) {
       return null
     } else if (key < this.key) {
-        this.left.remove(key)
-      } else if (key > this.key){
-        this.right.remove(key)
-    } else if (key === this.key) {//found the key
-      if (this.left && this.right) { //if there are two children
+      this.left.remove(key)
+    } else if (key > this.key) {
+      this.right.remove(key)
+    } else if (key === this.key) {
+      //found the key
+      if (this.left && this.right) {
+        //if there are two children
         const rightMin = this.right._findMin() //find the smallest key on the right side
         this.key = rightMin.key
         this.value = rightMin.value
         this.right.remove(rightMin.key)
-      } else if (this.left) { //if there is only a left child
+      } else if (this.left) {
+        //if there is only a left child
         this.left.parent = this.parent
         if (this.parent.left === this) this.parent.left = this.left
         if (this.parent.right === this) this.parent.right = this.left
-      } else if (this.right) { //if there is only a right child
+      } else if (this.right) {
+        //if there is only a right child
         this.right.parent = this.parent
         if (this.parent.left === this) this.parent.left = this.right
         if (this.parent.right === this) this.parent.right = this.right
-      } else { //if there are no children
+      } else {
+        //if there are no children
         if (this.parent.left === this) this.parent.left = null
         if (this.parent.right === this) this.parent.right = null
       }
@@ -73,18 +77,18 @@ class BinarySearchTree {
 
 function main() {
   let BST = new BinarySearchTree()
-  BST.insert({key: 3, value: null})
-  BST.insert({key: 1, value: null})
-  BST.insert({key: 4, value: null})
-  BST.insert({key: 6, value: null})
-  BST.insert({key: 9, value: null})
-  BST.insert({key: 2, value: null})
-  BST.insert({key: 5, value: 'something'})
-  BST.insert({key: 7, value: null})
+  BST.insert({ key: 3, value: null })
+  BST.insert({ key: 1, value: null })
+  BST.insert({ key: 4, value: null })
+  BST.insert({ key: 6, value: null })
+  BST.insert({ key: 9, value: null })
+  BST.insert({ key: 2, value: null })
+  BST.insert({ key: 5, value: 'something' })
+  BST.insert({ key: 7, value: null })
 
-  BST.remove(2)
+  BST.remove(7)
 
-  console.log(BST.find(5))
+  console.log(thirdLargest(BST))
 
   // BST.insert({key: 'E', value: null})
   // BST.insert({key: 'A', value: null})
@@ -99,7 +103,88 @@ function main() {
   // BST.insert({key: 'O', value: null})
   // BST.insert({key: 'N', value: null})
 
-  // console.log(BST)
+  // console.log(bstHeight(BST))
+  console.log(isBst(BST))
 }
 
 main()
+
+// 4.)
+// tree(t) adds all values in a tree
+function tree(t) {
+  // base case stop recursion if null
+  if (!t) {
+    return 0
+  }
+  // runs to the left adds the values of left and right and current
+  return tree(t.left) + t.value + tree(t.right)
+}
+
+// 5.)
+// Height of BST
+function bstHeight(bst) {
+  // run until null
+  if (!bst) {
+    return 0
+  }
+  let countL = bstHeight(bst.left)
+  let countR = bstHeight(bst.right)
+  // add one each recursion
+  if (countL > countR) {
+    return countL + 1
+  } else {
+    return countR + 1
+  }
+}
+
+// 6.) Is it a BST?
+function isBst(bst) {
+  if (!bst) {
+    return true
+  }
+  if (bst.right !== null && bst.right.key < bst.key) {
+    // console.log('Right Key:',bst.right.key)
+    return false
+  }
+  if (bst.left !== null && bst.left.key > bst.key) {
+    // console.log('Left Key:',bst.left.key)
+    return false
+  }
+  const bstL = isBst(bst.left)
+  const bstR = isBst(bst.right)
+  if (bstL && bstR) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// 7.) 3rd largest node
+function thirdLargest(bst) {
+  let largest = findLargest(bst)
+  if (largest.left) {
+    if (largest.left.right) {
+      return largest.left.right
+    }
+    if (largest.left.left) {
+      return largest.left.left
+    } else {
+      return largest.parent
+    }
+  } else {
+    if(largest.parent &&largest.parent.left){
+      return largest.parent.left
+    } else {
+      return largest.parent.parent
+    }
+  }
+}
+
+function findLargest(bst) {
+  if (bst.right === null) {
+    return bst
+  }
+  return findLargest(bst.right)
+}
+
+
